2adce9ffda5d8653f799145202b16fbfcfa87f15
ryanweston <ryanlweston@gmail.com>
2020-06-16 00:35:32 +0100

    modulised threejs + added glitch effect

--- public/js/3D.js
+++ public/js/3D.js
@@ -1,17 +1,25 @@
-
+import * as THREE from './three/build/three.module.js';
+import { GLTFLoader } from './three/examples/jsm/loaders/GLTFLoader.js';
+import { DRACOLoader } from './three/examples/jsm/loaders/DRACOLoader.js';
+import { EffectComposer } from './three/examples/jsm/postprocessing/EffectComposer.js';
+import { RenderPass } from './three/examples/jsm/postprocessing/RenderPass.js';
+import { GlitchPass } from './three/examples/jsm/postprocessing/GlitchPass.js';
 
 // Instantiate a loader
-var loader = new THREE.GLTFLoader();
+var loader = new GLTFLoader();
 // Optional: Provide a DRACOLoader instance to decode compressed mesh data
-var dracoLoader = new THREE.DRACOLoader();
+var dracoLoader = new DRACOLoader();
 dracoLoader.setDecoderPath('../decoder/');
+// dracoLoader.setDecoderConfig( { type: 'js' } );
 loader.setDRACOLoader(dracoLoader);
+
+
 
 const loadingScreen = document.getElementById("loading");
 const domWrapper = document.getElementById("wrapper");
 
 var model;
-camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 2, 1000);
+var camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 2, 1000);
 camera.position.y = 8;
 camera.position.z = 5;
 
@@ -24,12 +32,22 @@ document.body.appendChild(renderer.domElement);
 
 var scene = new THREE.Scene();
 
+var composer = new EffectComposer( renderer );
+
 //LIGHTS
 var light = new THREE.AmbientLight(0xffffff, 0.15);
 scene.add(light);
 
 var light2 = new THREE.PointLight(0xffffff, 1.5);
 scene.add(light2);
+
+var renderPass = new RenderPass( scene, camera );
+composer.addPass( renderPass );
+
+var glitchPass = new GlitchPass();
+composer.addPass( glitchPass );
+
+
 
 // Load a glTF resource
 loader.load(
@@ -39,17 +57,18 @@ loader.load(
     // called when the resource is loaded
     function (gltf) {
         model = gltf.scene;
-        // var material1 = new THREE.MeshStandardMaterial({ color: 0xFF0000 });
-        // model.traverse((o) => {
-        //     if (o.isMesh) {
-        //         console.log("there is a mesh");
-        //         // note: for a multi-material mesh, `o.material` may be an array,
-        //         // in which case you'd need to set `.map` on each value.
-        //         o.material = material1;
-        //     }
-        // })
+        var material1 = new THREE.MeshStandardMaterial({ wireframe: true });
+        model.traverse((o) => {
+            if (o.isMesh) {
+                console.log("there is a mesh");
+                // note: for a multi-material mesh, `o.material` may be an array,
+                // in which case you'd need to set `.map` on each value.
+                o.material = material1;
+            }
+        })
         scene.add(model);
-        render();
+        animate();
+        // dracoLoader.dispose();
     },
     // called while loading is progressing
     function (xhr) {
@@ -73,6 +92,8 @@ loader.load(
     }
 );
 
+
+
 window.addEventListener('resize', onWindowResize, false);
 
 function onWindowResize() {
@@ -81,10 +102,11 @@ function onWindowResize() {
     renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
-var render = function () {
-    requestAnimationFrame(render);
-    model.rotation.y = Date.now() * 0.0005;
-    // console.log("Date:" + Date.now() + " Rotation: " + model.rotation.y);
+function animate() {
+
+	requestAnimationFrame( animate );
+    model.rotation.y += 0.015;
     model.position.x = 2;
-    renderer.render(scene, camera);
-};
+	composer.render();
+
+}
