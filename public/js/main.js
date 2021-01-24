fbca91d0f2a8aed5e900da237fbd6517cbccb51a
Ryan Weston <ryanlweston@gmail.com>
2021-01-24 00:32:57 +0000

    yoinked w3 responsive menu + responsive

--- public/js/main.js
+++ public/js/main.js
@@ -1,23 +1,9 @@
-// const buttonRight = document.getElementById("scroll-right");
-// const buttonLeft = document.getElementById("scroll-left");
-// const element = document.getElementById("slide");
-
-// buttonRight.onclick = function () {
-//     element.scrollLeft += 310
-// }
-
-// buttonLeft.onclick = function () {
-//     element.scrollLeft -= 400
-// }
-
-
-// element.addEventListener("scroll", function (event) {
-//     var scroll = this.scrollLeft;
-
-//     if (scroll > 50) {
-//         buttonLeft.style.display = 'block';
-//     } else {
-//         buttonLeft.style.display = 'none';
-//     }
-// })
+function myFunction() {
+    var x = document.getElementById("myTopNav");
+    if (x.className === "topNav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topNav";
+    }
+  }
 
