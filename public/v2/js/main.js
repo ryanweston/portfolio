3a23f813b7fd9c8ea545a9b3cb54191ad51b61da
Ryan Weston <ryanlweston@gmail.com>
2021-01-28 23:54:14 +0000

    v2 white

---
+++ public/v2/js/main.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    var x = document.getElementById("myTopNav");
+    if (x.className === "topNav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topNav";
+    }
+  }
+
